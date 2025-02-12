e6a18794a3fbfb139b75ee61ad6fc5631ac15dfa
Floyd <zweydfg8+github@rub.de>
2025-02-12 21:58:52 +0100

    gitbak

--- linux-6.1.nix
+++ linux-6.1.nix
@@ -7,15 +7,15 @@ let
   modDirVersion = "6.6.36";
 in
 buildLinux (args // {
-  #inherit kernelPatches modDirVersion;
+    #inherit kernelPatches modDirVersion;
   inherit modDirVersion;
-  version = "${modDirVersion}-k1";
+  version = "${modDirVersion}";
   src = fetchgit {
     url = "https://github.com/BPI-SINOVOIP/pi-linux";
     rev = "7b25153f8744304c5212d1d4de4dbf51e9e2eb7e";
     hash = "sha256-dnB6Y+MfCBXmlSdnLPdAQOlLHYg9KmTIdV5MzgSk2YU=";
   };
-  kernelPatches = [
+  kernelPatches = kernelPatches ++ [
     {
       name = "fix_build";
       patch = ./kernel.patch;
@@ -26,9 +26,9 @@ buildLinux (args // {
   defconfig = "k1_defconfig";
   extraMeta.branch = "linux-6.6.36-k1";
   structuredExtraConfig = with lib.kernel; {
-    CMDLINE = freeform "console=ttyS0,115200 console=tty1";
-    CMDLINE_EXTEND = yes;
-    DISTRO_GENERIC_KERNEL = no;
+    # CMDLINE = freeform "console=ttyS0,115200 console=tty1";
+    # CMDLINE_EXTEND = yes;
+    # DISTRO_GENERIC_KERNEL = no;
     WLAN_VENDOR_REALTEK = no;
 
     SOC_SPACEMIT_K1X = yes;
@@ -43,5 +43,7 @@ buildLinux (args // {
     K1PRO_MAILBOX = no;
     K1PRO_REMOTEPROC = no;
     PWM_DWC_K1PRO = no;
+      
+    SPI_DESIGNWARE_EXT = no;
   };
 } // (args.argsOverride or { }))
